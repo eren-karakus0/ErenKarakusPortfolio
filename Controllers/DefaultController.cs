b0130bea81be2e03934649d869c009649df533f7
Muhammet Eren Karakuş <erenkar950@gmail.com>
2025-01-14 19:12:50 +0300

    Add project files.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMediaPortfolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+        
+        public PartialViewResult PartialSkill()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialAbout()
+        {
+            return PartialView();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScripts()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHeroSection()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialResumeSection()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialStats()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialPortfolio()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialTestimonial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialContact()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+    }
+    
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AcunMediaPortfolio.MvcApplication" Language="C#" %>
